b3725d926f20c97ca9098e0f2dee0051f6753726
Nameira Khalid Rana <57300076+nameirarana@users.noreply.github.com>
2022-03-05 16:45:23 +0500

    Market Place works

--- marketplace/marketplace.py
+++ marketplace/marketplace.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, render_template
 import grpc
-from recommendations_pb2 import BookCategory, RecommendationRequest
+from recommendations_pb2 import MovieCategory, RecommendationRequest
 from recommendations_pb2_grpc import RecommendationsStub
 app = Flask(__name__)
 recommendations_host = os.getenv("RECOMMENDATIONS_HOST", "localhost")
@@ -11,7 +11,7 @@ recommendations_client = RecommendationsStub(recommendations_channel)
 
 @app.route("/")
 def render_homepage():
-  recommendations_request = RecommendationRequest(user_id=1, category=BookCategory.MYSTERY, max_results=3)
+  recommendations_request = RecommendationRequest(user_id=1, category=MovieCategory.MYSTERY, max_results=3)
 
   recommendations_response = recommendations_client.Recommend(recommendations_request)
   return render_template("homepage.html", recommendations=recommendations_response.recommendations,)
