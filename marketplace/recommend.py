3f2608d2814f1704e06e9ee54df9f07f5c3ac67c
Nameira Khalid Rana <57300076+nameirarana@users.noreply.github.com>
2022-03-06 15:06:22 +0500

    Home and recommendation web pages

--- marketplace/recommend.py
+++ marketplace/recommend.py
@@ -8,23 +8,22 @@ recommendations_host = os.getenv("RECOMMENDATIONS_HOST", "localhost")
 recommendations_channel = grpc.insecure_channel(f"{recommendations_host}:50051")
 
 recommendations_client = RecommendationsStub(recommendations_channel)
+categories = ['Mystery','Science Fiction','Comedy','Thriller','Action']
 app = Flask(__name__)
 app.debug = True
 @app.route('/', methods=['GET'])
 def dropdown():
-    categories = ['Mystery', 'Thriller', 'Science Fiction', 'Action', 'Comedy']
+
     return render_template('recommendations.html', categories = categories)
 
 @app.route('/send_data', methods=['GET','POST'])
 def send_data():
     if request.method == "POST":
         searchterm = request.form['categories']
-        print(searchterm)
-        str = "MovieCategory." + searchterm.upper()
-        print(str)
-        recommendations_request = RecommendationRequest(user_id=1, category=MovieCategory.MYSTERY, max_results=20)
+        index = categories.index(searchterm)
+        recommendations_request = RecommendationRequest(user_id=1, category=index, max_results=20)
         recommendations_response = recommendations_client.Recommend(recommendations_request)
-        return render_template("homepage.html", recommendations=recommendations_response.recommendations, )
+        return render_template("show_recommendations.html", recommendations=recommendations_response.recommendations, )
     else:
         return render_template("recommendations.html")
 if __name__ == "__main__":
